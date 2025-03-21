70bfffe166e06ca8c723350167e4424cb05e327d
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-21 16:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Car(make, model) {
 		this.make = make;
 		this.model = model;
 }
-Car.prototype.getMakeModel = function(){
+getMakeModel(){
 		return `${this.make} ${this.model}`;
 };
 
@@ -11,10 +11,10 @@ function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model)
 	this.topSpeed = topSpeed;
 }
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
+SportsCar = Object.create(Car.prototype);
+SportsCar.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = function(){
+getTopSpeed = function(){
 	return this.topSpeed
 }
 
